3d8edf9f64ce2110b2c0ed3f043ce7000f3c3816
cooljasonmelton <cooljasonmelton>
2020-05-12 23:04:33 -0500

    figured out web scrapre

--- Gemfile
+++ Gemfile
@@ -42,3 +42,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'faker' 
 gem 'nokogiri'
+gem 'watir'
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,2 +1,24 @@
+require 'watir'
+require 'pp'
+
 class Prompt < ApplicationRecord
+
+    def self.scrape
+        prompt_array = []
+
+        browser = Watir::Browser.new
+        browser.goto 'https://thinkwritten.com/365-creative-writing-prompts'
+
+        # puts browser.title
+  
+        div = browser.div(:class => 'entry-content')
+        div.elements.each do |e|
+            if e.text[0].to_i > 0
+                prompt_array << e.text
+            end
+        end
+        browser.close
+        puts prompt_array
+    end 
+
 end
