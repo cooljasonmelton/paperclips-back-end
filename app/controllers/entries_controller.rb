e3ff618cb5ffc5581ba93000ef5854859a6af295
cooljasonmelton <cooljasonmelton>
2020-05-18 02:26:36 -0500

    date logic

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,8 @@
 class EntriesController < ApplicationController
+
+    def create
+        # selected_date = Date.parse(params[:date])
+        # entry = Entry.find_or_create_by(:created_at => selected_date.beginning_of_day..selected_date.end_of_day)      
+    end 
+
 end
