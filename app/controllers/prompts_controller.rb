222c6101cf75091ac6ee8b6206c7255011aff694
cooljasonmelton <cooljasonmelton>
2020-05-20 11:44:08 -0500

    set up to send random prompts

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,2 +1,10 @@
 class PromptsController < ApplicationController
+    skip_before_action :authorized, only: [:index]
+
+    def index
+        prompts = Prompt.all
+        render json: prompts.sample, only: [:content]
+    end 
+
+
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,5 +1,4 @@
 require 'watir'
-require 'pp'
 
 class Prompt < ApplicationRecord
 
@@ -14,11 +13,13 @@ class Prompt < ApplicationRecord
         div = browser.div(:class => 'entry-content')
         div.elements.each do |e|
             if e.text[0].to_i > 0
-                prompt_array << e.text
+                prompt_array << e.text.split(' ')[1..-1].join(' ')
             end
         end
         browser.close
-        puts prompt_array
+        prompt_array[0..-2].each do |p|
+            Prompt.create(content: p)
+        end 
     end 
 
 end
