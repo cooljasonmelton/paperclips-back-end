d7b14ceff93308fde45979c7b84a9f4f20849098
cooljasonmelton <cooljasonmelton>
2020-05-21 09:45:17 -0500

    prompt

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -2,7 +2,7 @@ require 'watir'
 
 class Prompt < ApplicationRecord
 
-    def self.scrape
+    def self.scrape_365_creative_writing_prompts
         prompt_array = []
 
         browser = Watir::Browser.new
@@ -21,5 +21,48 @@ class Prompt < ApplicationRecord
             Prompt.create(content: p)
         end 
     end 
+
+    def self.scrape_50_creative_nonfiction_prompts
+        prompt_array = []
+
+        browser = Watir::Browser.new
+        browser.goto 'https://thejohnfox.com/2016/06/creative-nonfiction-prompts/'
+
+        # puts browser.title
+  
+        div = browser.div(class: ['post-content', 'the-content'])
+        div.elements.each do |e|
+            if e.text[0].to_i > 0 && e.text != "750words"
+                prompt_array << e.text.split(' ')[1..-1].join(' ')
+            end
+        end
+        browser.close
+        prompt_array.uniq.each do |p|
+            Prompt.create(content: p)
+        end 
+    end 
+
+    def self.scrape_wattpad_1200_prompts_first_page
+        prompt_array = []
+
+        browser = Watir::Browser.new
+        browser.goto 'https://www.wattpad.com/47524188-1200-creative-writing-prompts-creative-nonfiction'
+
+        # puts browser.title
+  
+        div = browser.div(class: ['page'])
+        div.elements.each do |e|
+            if e.text[0].to_i > 0 && e.text
+                prompt_array << e.text.split(' ')[1..-1].join(' ')
+            end
+        end
+        browser.close
+        prompt_array.uniq.each do |p|
+            Prompt.create(content: p)
+        end 
+    end 
+
+
+
 
 end
